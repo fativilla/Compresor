be345e498064ba3a1a2b06a0e168f1787ad58b0e
fativilla <fativilla@gmail.com>
2024-08-28 19:23:32 -0400

    updatesVarios
    Cambios varios, mejoras en iconos y funcionalidad

--- Compresor.py
+++ Compresor.py
@@ -2,8 +2,9 @@ from PIL import Image
 import os
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
+import webbrowser
 
-def compress_image(input_path, output_path, quality=50):
+def compress_image(input_path, output_path, quality=60):
     """
     Comprime una imagen y la guarda en el directorio especificado.
     
@@ -16,7 +17,7 @@ def compress_image(input_path, output_path, quality=50):
             img = img.convert("RGB")
         img.save(output_path, "JPEG", quality=quality)
 
-def compress_images_in_folder(folder_path, output_folder, quality=50, progress_var=None, progress_bar=None):
+def compress_images_in_folder(folder_path, output_folder, quality=60, progress_var=None, progress_bar=None):
     # Asegurarse de que la carpeta de salida exista
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
@@ -41,11 +42,16 @@ def compress_images_in_folder(folder_path, output_folder, quality=50, progress_v
             progress_var.set(i)
             progress_bar.update()
 
+# Función para abrir el perfil de Instagram
+def abrir_instagram():
+    webbrowser.open("https://www.instagram.com/fatyvillaph/")
+
 def select_folder_and_compress():
     # Crear la ventana de progreso
     progress_window = tk.Toplevel()
     progress_window.title("Progreso")
     progress_window.geometry("300x100")
+    progress_window.iconbitmap('imag.ico')  # Establecer el ícono de la ventana principal
 
     # Variables para la barra de progreso
     progress_var = tk.IntVar()
@@ -53,7 +59,7 @@ def select_folder_and_compress():
     progress_bar.pack(pady=20, padx=20)
 
     # Abrir un cuadro de diálogo para seleccionar la carpeta
-    folder_path = filedialog.askdirectory(title="Selecciona la carpeta con las imágenes")
+    folder_path = filedialog.askdirectory(title="Selecciona la carpeta con las imágenes, deben ser .jpg")
     
     if folder_path:
         output_folder = os.path.join(folder_path, "archivoscomprimidos")
@@ -63,15 +69,20 @@ def select_folder_and_compress():
         progress_bar.config(maximum=total_files)
         
         # Comprimir todas las imágenes en la carpeta seleccionada
-        compress_images_in_folder(folder_path, output_folder, quality=50, progress_var=progress_var, progress_bar=progress_bar)
+        compress_images_in_folder(folder_path, output_folder, quality=60, progress_var=progress_var, progress_bar=progress_bar)
         progress_window.destroy()
-        messagebox.showinfo("Éxito", f"Todas las imágenes han sido comprimidas y guardadas en {output_folder}.")
+
+        # Mostrar mensaje y abrir perfil de Instagram al aceptar
+        if messagebox.showinfo("Éxito", f"Todas las imágenes han sido comprimidas y guardadas en {output_folder}.\nSi este programa te sirve, ¡puedes hacerme saber en @fatyvillaph en Instagram!"):
+            abrir_instagram()
     else:
         messagebox.showwarning("Cancelado", "No se seleccionó ninguna carpeta.")
 
 # Crear la ventana principal de tkinter
 root = tk.Tk()
 root.withdraw()  # Oculta la ventana principal
+root.iconbitmap('imag.ico')  # Establecer el ícono de la ventana principal
+root.title("Compresor de Imágenes")
 
 # Llamar a la función para seleccionar la carpeta y comprimir las imágenes
 select_folder_and_compress()
